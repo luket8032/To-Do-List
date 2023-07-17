fb04dcb727d52d59af7dab12e7c0da7e520aba8e
Luke <luket8032@hotmail.com>
2023-07-17 16:43:41 -0500

    Create Menu element and styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import './style.css'
 
-console.log('setup for todo list app')
+const menu = document.getElementById('menu');
+const menuBtn = document.getElementById('menuBtn')
+
+function closeMenu() {
+    menu.style.width = "0%";
+    menu.style.padding = "0px";
+    menuBtn.dataset.state = "close";
+};
+
+function openMenu () {
+    menu.style.width = "20%";
+    menu.style.padding = "16px";
+    menuBtn.dataset.state = "open";
+};
+
+menuBtn.addEventListener('click', () => {
+    switch(menuBtn.dataset.state) {
+        case "open":
+            closeMenu();
+            break
+        case "close":
+            openMenu();
+            break
+    }
+});
+
